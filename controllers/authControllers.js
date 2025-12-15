4ea53b735871854bc8c57cb4688b8c439aee7b72
Mariam-Sameh17 <Mariam.Abdelmotaleb05@eng-st.cu.edu.eg>
2025-12-15 04:07:42 +0200

    Fix: Update Prisma schema for MySQL

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -33,9 +33,9 @@ const finalResponse = (user, res, statusCode) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true,
+    //httpOnly: true,
   };
-  cookieOptions.secure = true;
+  // cookieOptions.secure = true;
 
   res.cookie('jwt', token, cookieOptions);
 
