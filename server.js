5214ec2f8a0a3196510c0a850ec1bfe7825f25bf
Mariam-Sameh17 <Mariam.Abdelmotaleb05@eng-st.cu.edu.eg>
2025-12-15 02:53:39 +0200

    Change app.listen to bind to 0.0.0.0

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ async function main() {
 
 main();
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
   console.log(`app running on port ${port}...`);
 });
